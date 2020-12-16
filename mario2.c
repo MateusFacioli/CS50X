6e95e9a1eddd5f3e2cedc7c16d91425ebf471058
Matt77727 <mateus_rf@hotmail.com>
2020-12-16 18:22:24 -0300

    Create mario2.c

---
+++ mario2.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i = 1, j = 1, espace = 1;
+
+    do
+    {
+        n = get_int("Height: "); //entrada usuario
+    }
+    while (n < 1 || n > 8); //validando entrada
+
+    for (i = 1; i <= n; i++) //linhas
+    {
+
+        for (j = 1; j <= n - espace; j++) //espaços
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= espace; j++)//triangulo  a direita
+        {
+            printf("#");
+        }
+        printf(" ");
+        printf(" ");
+        for (j = 1; j <= espace; j++) //triangulo a esquerda
+        {
+            printf("#");
+        }
+        printf("\n"); // outra linha
+        espace++; //aumentando o contador para linha seguinte imprimir + do #
+
+    }
+}
+
+
